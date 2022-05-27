f94dcf550ac7b69317c2e8f243a859a820b3fdab
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-27 22:49:19 +0300

    st:set height of images card on home page

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from . models import *
 # Create your views here.
 
 #home view rendering pictures of all category
 
 def home(req):
-    return render(req, 'photos/home.html' , {'title':home})
+    photos=Image.objects.all()
+    context={
+        'title':home,
+        'photos':photos
+    }
+    return render(req, 'photos/home.html' , context)
+
+
+
+def categoty_page(req):
+    return render(req, 'photos/test.html')
