b592db0b74913e1d3a09a740531d30d1ece7e613
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-27 15:16:15 +0300

    ft:add image, category and location module

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,29 @@
+from pyexpat import model
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    '''categories module'''
+    name=models.CharField(max_length=40, blank=True)
+
+
+
+
+class Location(models.Model):
+    '''locataion module'''
+    name=models.CharField(max_length=40, blank=True)
+
+
+class  Image(models.Model):
+    '''image module'''
+    name=models.CharField(max_length=40, blank=True)
+    decription=models.TextField()
+    image=models.ImageField(null=False, blank=True)
+    category=models.ForeignKey(Category)
+    location=models.ForeignKey(Category)
+
+
+    
