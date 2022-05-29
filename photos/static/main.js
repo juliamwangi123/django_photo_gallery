348bca95c79dd444a3161fbe8e3116d55a8fff36
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-29 22:26:30 +0300

    upload few more images

--- photos/static/main.js
+++ photos/static/main.js
@@ -1,17 +1,16 @@
 console.log('hello');
 
-let clip=document.getElementById('board')
-image=document.getElementById('clip')
+let clip=document.getElementById('board') //button to click when copying
+image=document.getElementById('clip') // get the image 
 
 console.log(clip)
 clip.addEventListener('click' ,(e) =>{
     e.preventDefault()
 
-    console.log('click');
-    let url=image.getAttribute('src')
+    let url=image.getAttribute('src') //get the image attribute url 
     
-    navigator.clipboard.writeText(url)
-    navigator.clipboard.readText().then(cliptext =>{
+    navigator.clipboard.writeText(url) //write the url to clipboard
+    navigator.clipboard.readText().then(cliptext =>{    //read the clipboard content
         alert(`${cliptext} copied to your clipboard`)
 
     })
