07230d7c2cf747324950ae8a3ee7fac1f1ff7ba1
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-29 12:42:53 +0300

    st:add a sub-menu  rendering to images in colleration to their categories and location

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns=[
     path('',  views.home, name='photos-home'),
     path('category/<str:pk>', views.category_page, name='category'),
-    # path('search/', views.search, name='search' )
 ]
