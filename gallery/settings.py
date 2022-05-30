0ce9bef00e54c33e37b8d76ec885c011ca7749d4
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-30 10:15:45 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR/ 'staticfiles')
+STATIC_ROOT= BASE_DIR/ 'staticfiles'
 
 
 STATIC_URL = 'static/'
@@ -125,10 +125,12 @@ STATIC_URL = 'static/'
 MEDIA_URL='images/'
 
 STATICFILES_DIRS=[
-   os.path.join(BASE_DIR/'static') 
+   BASE_DIR /'static'
 ]
 
-MEDIA_ROOT=BASE_DIR/'static/images'
+MEDIA_ROOT=BASE_DIR /'static/images'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
